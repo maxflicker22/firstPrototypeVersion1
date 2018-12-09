fe178372c9a73dbc736598b02ffaf7d83c716394
Manuel Plaschzug <manuelplaschzug@manuels-macbook-pro.local>
2018-12-09 20:36:47 +0100

    Added Login Screen (First test)

---
+++ prototype1scrollTableView/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  prototype1scrollTableView
+//
+//  Created by Manuel Plaschzug on 09.12.18.
+//  Copyright © 2018 Worldcare. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+    
+}
---
+++ prototype1scrollTableView/roundUIButton.swift
@@ -0,0 +1,16 @@
+//
+//  roundUIButton.swift
+//  prototype1scrollTableView
+//
+//  Created by Manuel Plaschzug on 09.12.18.
+//  Copyright © 2018 Worldcare. All rights reserved.
+//
+
+import UIKit
+
+class roundUIButton: UIButton {
+
+    btn.layer.cornerRadius = 10
+    btn.clipsToBounds = true
+
+}
