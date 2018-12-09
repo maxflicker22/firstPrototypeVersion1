66a78427927c27f19a82410ab770ba78025bf5b0
Markus Flicker <markusflicker@markuss-macbook-pro.local>
2018-12-09 12:13:23 +0100

    Initial Commit

---
+++ prototype1scrollTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  prototype1scrollTableView
+//
+//  Created by Markus Flicker on 09.12.18.
+//  Copyright © 2018 Worldcare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
